c58307b06293d8b7b9c8608bac31e5460e8aafec
Daniel-Olofsson <daniel-olofsson@hotmail.se>
2023-02-04 11:59:00 +0100

    last commit, merged branches

--- Models/Data.cs
+++ Models/Data.cs
@@ -3,5 +3,11 @@
     public class Data
     {
         public string activity { get; set; }
+        /*public string type { get; set; }
+        public int participants { get; set; }
+        public float price { get; set; }
+        public string link { get; set; }
+        public string key { get; set; }
+        public float accessibility { get; set; }*/
     }
 }
